2b0df44aad3802a8395360d21e33d934cdefb450
Geovane Rodrigues <66053162+geovanerodrigue@users.noreply.github.com>
2022-05-13 10:16:30 -0300

    first commit

---
+++ Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lanhouse.Hydra.Models
+{
+    public class Usuario
+    {
+
+        public int  Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public int Telefone { get; set; }
+    }
+}
---
+++ Models/UsuarioContexto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lanhouse.Hydra.Models
+{
+    public class UsuarioContexto : DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+
+        public UsuarioContexto()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "Server=localhost;Database=lanhouse;Uid=root;Pwd=180897",
+                new MySqlServerVersion(new Version(8, 0, 11))
+            );
+        }
+    }
+}
